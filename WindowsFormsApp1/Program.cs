1bdb3149db66bd475e42d18602b66e61f8f45665
FEnikSO <FEnikSO@DESKTOP-487PNHB>
2022-07-27 02:13:48 +0300

    v1.0

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsApp1
     internal static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// Главная точка входа для приложения.//
         /// </summary>
         [STAThread]
         static void Main()
